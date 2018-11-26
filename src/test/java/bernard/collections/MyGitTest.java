9e893a20167fc338531fef76420919bcf852aa7f
buddhadev <buddhadevbunu@gmail.com>
2018-11-26 17:39:53 +0530

    added for test

--- src/test/java/bernard/collections/MyGitTest.java
+++ src/test/java/bernard/collections/MyGitTest.java
@@ -1,4 +1,14 @@
+package bernard.collections;
+
+import org.junit.jupiter.api.Test;
+
 public class MyGitTest{
+
+
+    @Test
+    void testGitMethod(){
+        System.out.println("my file name ");
+    }
 
 
 
